df2944f4a8ff7d17d51a9f9db44494f8395ef23d
Niks-World <nikkalyan52@gmail.com>
2023-09-05 22:06:17 +0530

    squre

--- HOF/square.js
+++ HOF/square.js
@@ -1,6 +1,15 @@
+//Given an array of numbers print the square of those numbers
+
 var sq=[1,2,3,4,5];
 
-var out= forEach(element => {
-    return ele ** 2;
+sq.forEach(function(ele,) {
+    // return ele*ele;
+    console.log(ele * ele);
 });
-console.log(out);
+ // output is the squre of givin element
+/* 1
+4
+9
+16
+25 */
+
