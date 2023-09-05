4b4428d2284eb1a21d1bb42adf574b8b637235e2
Niks-World <nikkalyan52@gmail.com>
2023-09-05 22:11:29 +0530

    double the value

---
+++ HOF/double.js
@@ -0,0 +1,10 @@
+/* Given an array of numbers generate an array containing the double value of the numbers
+
+Sample Input [1, 2] Sample Output [2, 4] */
+
+var nums= [1,2];
+
+nums.forEach(function(ele,index)
+{
+    console.log(ele + ele);
+});
