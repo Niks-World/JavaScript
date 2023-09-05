0287b36e14b574047a72a99be2c54b42c49e79a6
Niks-World <nikkalyan52@gmail.com>
2023-09-05 22:17:49 +0530

    double the value

--- HOF/double.js
+++ HOF/double.js
@@ -2,9 +2,28 @@
 
 Sample Input [1, 2] Sample Output [2, 4] */
 
-var nums= [1,2];
+var nums= [1,2,3,4,5,6];
 
 nums.forEach(function(ele,index)
 {
-    console.log(ele + ele);
+    console.log(ele *ele);  
 });
+/*
+2
+4
+6
+8
+10
+12
+*/
+
+// we can also use map 
+
+var nums=[3,5,6,7,8,9]
+
+var out = nums.map(function(ele){
+    return ele*2;
+})
+console.log(out);
+
+// [ 6, 10, 12, 14, 16, 18 ]
