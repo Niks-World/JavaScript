ce2fd1fc4a54771e076416dabcef12e88c4e7b81
Niks-World <nikkalyan52@gmail.com>
2023-09-10 03:10:50 +0530

    Todo script

---
+++ DOM-II/ToDo/index.js
@@ -0,0 +1,67 @@
+document.querySelector("form").addEventListener("submit", todofun);
+ var todoarr=JSON.parse(localStorage.getItem("todo"))||[];
+
+
+ displaytodo();
+ function displaytodo(){
+    document.querySelector("tbody").textContent="";
+    todoarr.map(function(ele,i){
+        var tr= document.createElement("tr");
+
+    var td1= document.createElement("td");
+    td1.textContent= ele.task;
+    var td2= document.createElement("td");
+    td2.textContent= ele.priority;
+    if(ele.priority==="High"){
+        td2.style.backgroundColor="red";
+    }else{
+        td2.style.backgroundColor="green"
+    }
+    var td3= document.createElement("td");
+    td3.textContent="delete"
+    
+    td3.addEventListener("click",function(){
+        deletrow(i);
+    });
+
+    tr.append(td1,td2,td3);
+    document.querySelector("tbody").append(tr);
+    })
+ }
+function todofun(){
+    event.preventDefault();
+    var task= document.getElementById("task").value;
+    var priority=  document.getElementById("priority").value;
+
+   var todo= {task:task, priority:priority};
+   todoarr.push(todo);
+   localStorage.setItem("todo", JSON.stringify(todoarr))
+
+   
+    var tr= document.createElement("tr");
+
+    var td1= document.createElement("td");
+    td1.textContent= task;
+    var td2= document.createElement("td");
+    td2.textContent= priority;
+    if(priority==="High"){
+        td2.style.backgroundColor="red";
+    }else{
+        td2.style.backgroundColor="green"
+    }
+    var td3= document.createElement("td");
+    td3.textContent="delete"
+    
+    td3.addEventListener("click",deletrow);
+
+    tr.append(td1,td2,td3);
+    document.querySelector("tbody").append(tr);
+    displaytodo();
+}
+
+function deletrow(i){
+    // event.target.parentNode.remove();
+    var res= todoarr.splice(i,1);
+    localStorage.setItem("todo",JSON.stringify(todoarr));
+    displaytodo();
+}
