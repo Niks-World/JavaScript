09510c041c8a4f284eb6c9db646d35fc1a63bc4d
Niks-World <nikkalyan52@gmail.com>
2023-09-20 14:27:56 +0530

    bookmar delete fnc work

---
+++ Question CP/JS Basic Throw Dice/src/throwDice.js
@@ -0,0 +1,22 @@
+const memberA = () => {};
+
+const memberB = () => {};
+
+const memberC = () => {};
+
+const findWinner = (val1, val2, val3) => {};
+
+window.onload = function () {
+  //  get the button here and add click event
+};
+
+// donot change the export statement
+
+if (typeof exports !== "undefined") {
+  module.exports = {
+    memberA,
+    memberB,
+    memberC,
+    findWinner,
+  };
+}
